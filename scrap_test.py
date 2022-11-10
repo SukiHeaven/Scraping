84d917f06ac574778b04704617831f2911c5e058
SukiHeaven <72069106+SukiHeaven@users.noreply.github.com>
2022-11-10 15:08:15 +0100

    scraping

--- scrap_test.py
+++
@@ -1,9 +0,0 @@
-import urllib
-import bs4
-
-url_ligue_1 = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2019-2020"
-    
-from urllib import request
-
-request_text = request.urlopen(url_ligue_1).read()
-print(request_text[:1000])
